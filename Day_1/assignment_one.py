f6155bab0b789a0fd7c16ab0b19f99516145874a
D3m0nC0d3r <58410827+D3m0nC0d3r@users.noreply.github.com>
2021-08-17 16:31:00 +0530

    Day 1 Assignment submitted
    # Assignment - Take two inputs and store in variable x and y, find x to the power y.

---
+++ Day_1/assignment_one.py
@@ -0,0 +1,7 @@
+# Assignment - Take two inputs and store in variable x and y, find x to the power y.
+
+x = int(input("Enter first value: "))
+y = int(input("Enter second value: "))
+
+pwr_value = x**y
+print(pwr_value)
